17a24d833fe69ee83d23f73e6336d361f6cc2933
Daniel S <devolution.sdc@gmail.com>
2023-09-06 17:58:45 -0500

    tc 4 in sprint 4

--- src/Sprint_02/tc_03.java
+++ src/Sprint_02/tc_03.java
@@ -3,6 +3,6 @@ package Sprint_02;
 public class tc_03 {
     public static void main(String[] args) {
         System.out.println("the test case 3 from sprint  2 ...");
-        System.out.println("change on tc 3 of sprint 2");
+        System.out.println("change on tc 3 of sprint 2  ");
     }
 }
