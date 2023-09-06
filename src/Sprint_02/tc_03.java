1525b27461d6b78f5b6fb3dbfe7d053a30f0d681
Daniel S <devolution.sdc@gmail.com>
2023-09-06 17:41:41 -0500

    commit tc3 in sprint 2

--- src/Sprint_02/tc_03.java
+++ src/Sprint_02/tc_03.java
@@ -2,6 +2,6 @@ package Sprint_02;
 
 public class tc_03 {
     public static void main(String[] args) {
-        System.out.println("the test case 3 from sprint 2");
+        System.out.println("the test case 3 from sprint  2 ...");
     }
 }
