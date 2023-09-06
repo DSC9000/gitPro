811b39ede37c1882b3a8d256b2a4e4b45c27877d
Daniel S <devolution.sdc@gmail.com>
2023-09-06 17:26:07 -0500

    tc 1 and tc 2 and git ignore

---
+++ src/Sprint_01/tc01.java
@@ -0,0 +1,7 @@
+package Sprint_01;
+
+public class tc01 {
+    public static void main(String[] args) {
+        System.out.println("tc 1 and here we go");
+    }
+}
---
+++ src/Sprint_01/tc02.java
@@ -0,0 +1,7 @@
+package Sprint_01;
+
+public class tc02 {
+    public static void main(String[] args) {
+        System.out.println("this is tc 2");
+    }
+}
