3166abd10348a6fd43f71c964de32841177d2127
AVA <avaaflak@gmail.com>
2023-09-10 19:16:05 -0700

    added tc05

---
+++ src/Sprint_03/TC05.java
@@ -0,0 +1,9 @@
+package Sprint_03;
+
+public class TC05 {
+
+    public static void main(String[] args) {
+
+        System.out.println("this is test case 5 in sprint 3 from cloned project");
+    }
+}
