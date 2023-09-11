47a70f520c2b40d56ad2838d3a5ede7ca191af71
abdel <abdelazizouberka415@gmail.com>
2023-09-11 00:00:04 -0400

    new User story

---
+++ src/Sprint_01/UserStory13.java
@@ -0,0 +1,15 @@
+package Sprint_01;
+
+public class UserStory13 {
+
+    public static void main(String[] args) {
+        String str = "abccdew";
+        if(str.length() > 1){
+            String str1 = str.substring(0,2);
+
+        }
+
+    }
+
+}
+
